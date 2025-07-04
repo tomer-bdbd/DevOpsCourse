8f476e28d4dbd869e8ca4353e44130447db1e662
Tomer BD <tomerbd@tomerbd.tech>
2025-07-04 15:38:50 +0000

    edited pod yaml and the py app

--- getuser/getUser.py
+++ getuser/getUser.py
@@ -1,3 +1,22 @@
 import getpass
+import signal
+import sys
 
-print(getpass.getuser())
+username = getpass.getuser()
+print(f"Hello! You are {username}")
+print("Application running... Use 'kubectl delete pod' to stop")
+
+
+def signal_handler(signum, frame):
+    print(f"Received signal {signum}. Shutting down...")
+    sys.exit(0)
+
+
+signal.signal(signal.SIGTERM, signal_handler)
+signal.signal(signal.SIGINT, signal_handler)
+
+try:
+    signal.pause()
+except KeyboardInterrupt:
+    print("Shutting down...")
+
